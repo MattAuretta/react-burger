bde7bfaf8ff6118dcaf8ff12605ae46b93ae53bd
Matt Auretta <mauretta2@gmail.com>
2019-02-23 19:34:28 -0700

    dynamic ingredients state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,24 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    // Extracts keys from an object (ingredients in BurgerBuilder state) and transforms the values into an array
+    const transformedIngredients = Object.keys(props.ingredients)
+    //Map over each ingredient as a string value
+    .map(igKey => {
+        // Returns an array with a length equal to the ingredient's state value
+        // [...Array(3)] would return an array with 3 undefined spaces
+        // We use props.ingredients[igKey] to get the specific number for said ingredient
+        return [...Array(props.ingredients[igKey])]
+        // Map the undefined values array
+        // We use an underscore since the value doesn't matter, but we do want the index
+        .map((_, index) => {
+            return <BurgerIngredient key={igKey + index} type={igKey} />;
+        });
+    });
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
